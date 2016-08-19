171c04373f5d32e0417e5aea9881aa33cedded6f
Allan Cabusora <yiu.ascex@gmail.com>
2016-08-19 21:42:09 +0800

    Update SQL Database

--- sjrdc_steel.sql
+++ sjrdc_steel.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50626
 File Encoding         : 65001
 
-Date: 2016-08-11 22:40:00
+Date: 2016-08-19 21:41:43
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -25,7 +25,7 @@ CREATE TABLE `coils` (
   `coil_length` decimal(11,2) DEFAULT NULL,
   `coil_height` decimal(11,2) DEFAULT NULL,
   `coil_width` decimal(11,2) DEFAULT NULL,
-  `coil_color_id` bigint(20) DEFAULT NULL,
+  `coil_clr_id` bigint(20) DEFAULT NULL,
   `coil_created_by` bigint(20) DEFAULT NULL,
   `coil_created_date` datetime DEFAULT NULL,
   PRIMARY KEY (`coil_id`)
@@ -33,6 +33,21 @@ CREATE TABLE `coils` (
 
 -- ----------------------------
 -- Records of coils
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `colors`
+-- ----------------------------
+DROP TABLE IF EXISTS `colors`;
+CREATE TABLE `colors` (
+  `clr_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `clr_name` varchar(100) DEFAULT NULL,
+  `clr_hex` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`clr_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of colors
 -- ----------------------------
 
 -- ----------------------------
@@ -73,6 +88,74 @@ CREATE TABLE `employees` (
 -- Records of employees
 -- ----------------------------
 INSERT INTO `employees` VALUES ('1', '1', 'admin', '21232f297a57a5a743894a0e4a801fc3', '1', '10000.00');
+
+-- ----------------------------
+-- Table structure for `purchase_orders`
+-- ----------------------------
+DROP TABLE IF EXISTS `purchase_orders`;
+CREATE TABLE `purchase_orders` (
+  `po_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `po_date` date DEFAULT NULL,
+  `po_delivery_date` date DEFAULT NULL,
+  `po_terms` varchar(100) DEFAULT NULL,
+  `po_cust_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`po_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of purchase_orders
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `purchase_order_details`
+-- ----------------------------
+DROP TABLE IF EXISTS `purchase_order_details`;
+CREATE TABLE `purchase_order_details` (
+  `pod_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `pod_qty` decimal(11,2) DEFAULT NULL,
+  `pod_price` decimal(11,2) DEFAULT NULL,
+  `pod_sht_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`pod_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of purchase_order_details
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `sheets`
+-- ----------------------------
+DROP TABLE IF EXISTS `sheets`;
+CREATE TABLE `sheets` (
+  `sht_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `sht_coil_Id` bigint(20) DEFAULT NULL,
+  `sht_code` varchar(100) DEFAULT NULL,
+  `sht_length` decimal(11,2) DEFAULT NULL,
+  `sht_height` decimal(11,2) DEFAULT NULL,
+  `sht_width` decimal(11,2) DEFAULT NULL,
+  `sht_clr_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`sht_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of sheets
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `suppliers`
+-- ----------------------------
+DROP TABLE IF EXISTS `suppliers`;
+CREATE TABLE `suppliers` (
+  `supp_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `supp_ui_id` bigint(20) DEFAULT NULL,
+  `supp_company` varchar(100) DEFAULT NULL,
+  `supp_status` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`supp_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of suppliers
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for `user_informations`
